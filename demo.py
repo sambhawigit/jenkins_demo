0f78025a5af65cfad0c73125c58fc057ddaf653a
sambhawigit <107303321+sambhawigit@users.noreply.github.com>
2022-06-15 15:52:50 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ msg="Hello World!"
 print(msg)
 print("This will trigger the pipeline!")
 print("Trigger pipeline")
+print("pushing from feature branch")
 # commit-change
 # change
 # run this script
